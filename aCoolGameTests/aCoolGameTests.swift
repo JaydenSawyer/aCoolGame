4feb65aa7d5acf872966f7bd7d00f40daf178778
Jayden Sawyer <jayden.sawyer25@d155.org>
2025-02-20 07:29:11 -0600

    Initial Commit

---
+++ aCoolGameTests/aCoolGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  aCoolGameTests.swift
+//  aCoolGameTests
+//
+//  Created by JAYDEN SAWYER on 2/20/25.
+//
+
+import Testing
+@testable import aCoolGame
+
+struct aCoolGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
